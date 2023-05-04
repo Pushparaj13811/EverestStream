6240688bff571b3db2b6076f57cec9f0d48e253a
Hompushparaj mehta <115880885+Pushparaj1381@users.noreply.github.com>
2023-05-05 00:03:06 +0530

    Created scrolleffect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,6 +18,36 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const controlNavbar = () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate("/search/${query}");
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  };
+
   const openSearch = () => {
     setMobileMenu(false);
     setShowSearch(true);
@@ -27,22 +57,34 @@ const Header = () => {
     setShowSearch(false);
   };
 
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+    setMobileMenu(false);
+  };
+
   return (
-    <Header className={`header ${mobileMenu ? "mobileView" 
-    : ""} ${show}`}>
+    <Header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
         <div className="logo">
           <img src={logo} alt="" />
         </div>
         <ul className="menuItems">
-          <li className="menuItem">Movies</li>
-          <li className="menuItem">Tv Shows</li>
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("rv")}>
+            Tv Shows
+          </li>
           <li className="menuItem">
-            <HiOutlineSearch />
+            <HiOutlineSearch onClick={openSearch} />
           </li>
         </ul>
         <div className="mobileMenuItems">
-          <HiOutlineSearch />
+          <HiOutlineSearch onClick={openSearch} />
           {mobileMenu ? (
             <VscChromeClose onClick={() => setMobileMenu(false)} />
           ) : (
@@ -50,6 +92,23 @@ const Header = () => {
           )}
         </div>
       </ContentWrapper>
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Search for movie or tv show....."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
     </Header>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './style.scss';
-import HeroBanner from './heroBanner/HeroBanner';
+import React from "react";
+import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
 
 const home = () => {
   return (
-    <div className='homePage'>
-        <HeroBanner/>   
+    <div className="homePage">
+      <HeroBanner />
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 };
 
-export default home
+export default home;
